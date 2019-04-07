b8f109ab45230f7f8ff3c79bf445fceea7135818
Jasper Swallen <swallen@usc.edu>
2019-04-07 14:35:06 -0700

    add logout

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ CORS(app)
 
 # global variable to save our access_token
 access = None
+loggedin = False
 
 client = smartcar.AuthClient(
     client_id=os.environ.get('CLIENT_ID'),
@@ -22,25 +23,36 @@ client = smartcar.AuthClient(
 @app.route('/login', methods=['GET'])
 def login():
     auth_url = client.get_auth_url()
+    global loggedin
+    loggedin = True
     return redirect(auth_url)
 
 
 @app.route('/exchange', methods=['GET'])
 def exchange():
     code = request.args.get('code')
+    global loggedin
 
+    if not loggedin:
+        return 'Sorry, access denied. Please log in first', 401
+    
     # access our global variable and store our access tokens
     global access
     # in a production app you'll want to store this in some kind of
     # persistent storage
     access = client.exchange_code(code)
-    return '', 200
+    return 'got access codes', 200
 
 
 @app.route('/vehicle', methods=['GET'])
 def vehicle():
     # access our global variable to retrieve our access tokens
     global access
+    global loggedin
+
+    if not loggedin:
+        return 'no vehicles', 400
+
     # the list of vehicle ids
     vehicle_ids = smartcar.get_vehicle_ids(
         access['access_token'])['vehicles']
@@ -51,25 +63,47 @@ def vehicle():
     info = vehicle.info()
     print(info)
 
-    return jsonify(info)
+    return jsonify(info), 200
 
 @app.route('/unlock', methods=['GET'])
 def unlockCar():
     global access
+    global loggedin
+    
+    if not loggedin:
+        return 'no vehicles', 401
+
     vehicle_ids = smartcar.get_vehicle_ids(
         access['access_token'])['vehicles']
     
     vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
 
-    status = 'false'
+    vehicle.unlock()
+    print('unlocked')
 
-    if vehicle.unlock():
-        status = 'true'
-        print('unlocked')
-    else:
-        print('not unlocked')
+    return 'unlocked!', 200
 
-    return jsonify(status)
+@app.route('/logout', methods=['GET'])
+def logout():
+    global access
+    global loggedin
+
+    if not loggedin:
+        print('no vehicles to disconnect')
+        return 'no vehicles to disconnect', 200
+    
+
+    vehicle_ids = smartcar.get_vehicle_ids(
+        access['access_token'])['vehicles']
+    
+    for vehicle_id in vehicle_ids:
+        vehicle = smartcar.Vehicle(vehicle_id, access['access_token'])
+        vehicle.disconnect()
+        print('disconnected')
+
+    loggedin = False
+
+    return 'disconnected', 200
 
 
 if __name__ == '__main__':
